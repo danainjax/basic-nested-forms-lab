79fb3be74e8e7667434a78f475b0ea6f52ff7baa
danainjax <danainjax@gmail.com>
2021-04-06 10:27:28 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,21 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(name: "Main Protein")
+    @recipe.ingredients.build(name: "I Could Literally Type in Bring Back Avi Here")
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(@recipe)
   end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, ingredients_attributes: [ 
+      :name,
+      :quantity
+      ]
+    )
+  end
+
 end
